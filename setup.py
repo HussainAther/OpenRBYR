cbe0a6face18961c982c4fa17da589320141d00c
Syed Hussain Ather <shussainather@gmail.com>
2025-05-31 01:21:23 -0400

    metadata

--- setup.py
+++ setup.py
@@ -1,21 +1,24 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="openrbyr",
-    version="0.1.0",
+    name='openrbyr',
+    version='0.1.0',
     packages=find_packages(),
     install_requires=[
-        "numpy",
-        "matplotlib"
+        'streamlit',
+        'torch',
+        'opencv-python',
+        # Add other dependencies here
     ],
-    author="Hussain Ather",
-    description="A Ray-by-Ray CT Simulation Toolkit",
-    license="MIT",
-    url="https://github.com/HussainAther/OpenRBYR",
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='OpenRBYR: Industrial CT Defect Detection Dashboard',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/HussainAther/OpenRBYR',
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
     ],
 )
 
